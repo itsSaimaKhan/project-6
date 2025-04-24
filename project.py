37b837920ef4bfd7504fa55537dc91d2271d722f
itssaimaKhan <samiakhan8754@gmail.com>
2025-04-25 01:24:35 +0500

    main

---
+++ project.py
@@ -0,0 +1,44 @@
+# Project 6: Buuild_Compose_and_Decorate_A_Complete_Traditional_OOP_Practice_Series
+# Instructor: Saima Khan
+
+# 1. Using self
+# Assignment:
+# Create a class Student with attributes name and marks. Use the self keyword to initialize
+# these values via a constructor. Add a method display() that prints student details.
+
+#solution:
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print(f" Student Name: {self.name}")
+        print(f" Student Marks: {self.marks}")
+        
+student1 = Student("saima", 85)   
+student1.display()     
+
+
+# 2. Using cls
+# Assignment:
+# Create a class Counter that keeps track of how many objects have been created. Use a class variable
+# and a class method with cls to manage and display the count.
+
+#solution:
+class Counter:
+    count = 0
+
+    def __init__(self):
+        Counter.count += 1
+
+    @classmethod
+    def display_count(cls):
+        print(f"MY total created objects are: {cls.count}")
+        
+obj1 = Counter()
+obj2 = Counter()
+obj3 = Counter()
+obj4 = Counter()
+
+Counter.display_count()  # Output: Total objects created: 4        
