de47d7d7dd25f898569cc1a4a2d1607830c9c79d
itssaimaKhan <samiakhan8754@gmail.com>
2025-04-25 23:32:19 +0500

    main

--- project.py
+++ project.py
@@ -7,17 +7,17 @@
 # these values via a constructor. Add a method display() that prints student details.
 
 #solution:
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+# class Student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
 
-    def display(self):
-        print(f" Student Name: {self.name}")
-        print(f" Student Marks: {self.marks}")
+#     def display(self):
+#         print(f" Student Name: {self.name}")
+#         print(f" Student Marks: {self.marks}")
         
-student1 = Student("saima", 85)   
-student1.display()     
+# student1 = Student("saima", 85)   
+# student1.display()     
 
 
 # 2. Using cls
@@ -26,19 +26,36 @@ student1.display()
 # and a class method with cls to manage and display the count.
 
 #solution:
-class Counter:
-    count = 0
+# class Counter:
+#     count = 0
 
-    def __init__(self):
-        Counter.count += 1
+#     def __init__(self):
+#         Counter.count += 1
 
-    @classmethod
-    def display_count(cls):
-        print(f"MY total created objects are: {cls.count}")
+#     @classmethod
+#     def display_count(cls):
+#         print(f"MY total created objects are: {cls.count}")
         
-obj1 = Counter()
-obj2 = Counter()
-obj3 = Counter()
-obj4 = Counter()
+# obj1 = Counter()
+# obj2 = Counter()
+# obj3 = Counter()
+# obj4 = Counter()
 
-Counter.display_count()  # Output: Total objects created: 4        
+# Counter.display_count()  # Output: Total objects created: 4    
+
+# 3. Public Variables and Methods
+# Assignment:
+# Create a class Car with a public variable brand and a public method start(). Instantiate 
+# the class and access both from outside the class. 
+
+#solution:
+# class Car:
+#     def __init__(self, brand):
+#         self.brand = brand
+
+#     def start(self):
+#         print(f"{self.brand} is starting.")
+        
+# my_car = Car("Toyota")
+# print(my_car.brand)  # Accessing public variable
+# my_car.start()  # Calling public method           
