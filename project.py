7a0203c0d1b9b74cfaecbb3d0b2e10433c877cea
itssaimaKhan <samiakhan8754@gmail.com>
2025-05-01 23:34:35 +0500

    main

--- project.py
+++ project.py
@@ -49,12 +49,12 @@
 # the class and access both from outside the class. 
 
 # solution:
-class Car:
-    def __init__(self, brand):
-        self.brand = brand
+# class Car:
+#     def __init__(self, brand):
+#         self.brand = brand
 
-    def start(self):
-        print(f"{self.brand} is starting.")
+#     def start(self):
+#         print(f"{self.brand} is starting.")
         
 # my_car = Car("Toyota")
 # print(my_car.brand)  # Accessing public variable
