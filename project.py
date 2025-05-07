dffa2aadf050eb5004fc3b0b24514765694ffc10
itssaimaKhan <samiakhan8754@gmail.com>
2025-05-07 23:35:50 +0500

    main

--- project.py
+++ project.py
@@ -48,13 +48,13 @@
 # Create a class Car with a public variable brand and a public method start(). Instantiate 
 # the class and access both from outside the class. 
 
-# solution:
-# class Car:
-#     def __init__(self, brand):
-#         self.brand = brand
+solution:
+class Car:
+    def __init__(self, brand):
+        self.brand = brand
 
-#     def start(self):
-#         print(f"{self.brand} is starting.")
+    def start(self):
+        print(f"{self.brand} is starting.")
         
 my_car = Car("Toyota")
 print(my_car.brand)  # Accessing public variable
