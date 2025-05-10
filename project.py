c486d5b0d22f2af82dc0ce929fc3be3acb9f8a48
itssaimaKhan <samiakhan8754@gmail.com>
2025-05-10 23:57:52 +0500

    main

--- project.py
+++ project.py
@@ -7,17 +7,17 @@
 # these values via a constructor. Add a method display() that prints student details.
 
 # solution:
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+# class Student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
 
-    def display(self):
-        print(f" Student Name: {self.name}")
-        print(f" Student Marks: {self.marks}")
+#     def display(self):
+#         print(f" Student Name: {self.name}")
+#         print(f" Student Marks: {self.marks}")
         
-student1 = Student("saima", 85)   
-student1.display()     
+# student1 = Student("saima", 85)   
+# student1.display()     
 
 
 # 2. Using cls
