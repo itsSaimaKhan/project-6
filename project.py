219fc3b69589991335e80bca033a596c5bd891a1
itssaimaKhan <samiakhan8754@gmail.com>
2025-05-10 23:55:57 +0500

    main

--- project.py
+++ project.py
@@ -6,18 +6,18 @@
 # Create a class Student with attributes name and marks. Use the self keyword to initialize
 # these values via a constructor. Add a method display() that prints student details.
 
-# solution:
-# class Student:
-#     def __init__(self, name, marks):
-#         self.name = name
-#         self.marks = marks
+solution:
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
 
-#     def display(self):
-#         print(f" Student Name: {self.name}")
-#         print(f" Student Marks: {self.marks}")
+    def display(self):
+        print(f" Student Name: {self.name}")
+        print(f" Student Marks: {self.marks}")
         
-# student1 = Student("saima", 85)   
-# student1.display()     
+student1 = Student("saima", 85)   
+student1.display()     
 
 
 # 2. Using cls
